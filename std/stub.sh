bfef1b4edb55619d4d4bf38441f88baf1ce45fcd
Andrey Yaromenok <ayaromenok@gmail.com>
2020-11-22 16:10:49 +0200

    stub scad file added

---
+++ std/stub.sh
@@ -0,0 +1,8 @@
+include <../lib/lib2.scad>
+stub();
+module stub_}(px=0, py=0, pz=0, rx=0, ry=0, rz=0){
+    translate([(px), (py), pz])
+    rotate([rx,ry,rz]){
+	yCube();
+	}//transform
+}//module
